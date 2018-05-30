425482a251f0d63a0da31cdb7269ea66ca497630
vishal <vishal4790@gmail.com>
2018-05-30 08:44:43 -0400

    2nd video complete

---
+++ python_practice/numbers.py
@@ -0,0 +1,17 @@
+
+num = 3
+print(type(num))
+
+num1 = 2.2
+print(type(num1))
+
+print(abs(-3))
+print(round(3.5))
+print(round(7.38,1))
+print(round(-3.5))
+
+num_1 = '100'
+num_2 = '200'
+
+print(num_1 + num_2)
+print(int(num_1) + int(num_2))
