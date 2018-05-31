97be14e35b0b905c8e5b386e814da3ac9bb61060
vishal <vishal4790@gmail.com>
2018-05-31 09:56:02 -0400

    dictonaries complete

---
+++ python_practice/conditional.py
@@ -0,0 +1,19 @@
+#Conditional and booleans
+
+language = 'python'
+
+if 'python' is 'python':
+	print('true')
+
+user = True
+
+if language == 'python' and user:
+	print('and condition met')
+
+if not user:
+	print('you are not a user')
+else:
+	print('you are a user')
+
+print(id('python'))
+print(id(language))
