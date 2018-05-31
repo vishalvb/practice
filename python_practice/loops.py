9070f0915dbd708732cbf5aa14ae070dc7e4cbee
vishal <vishal4790@gmail.com>
2018-05-31 10:02:56 -0400

    loops

---
+++ python_practice/loops.py
@@ -0,0 +1,13 @@
+#Loops and Iterations
+
+nums = [1,2,3,4,5]
+for num in nums:
+	if num == 3:
+		#print('its three')
+		continue
+	else:
+		print(num)
+	
+print('range function')
+for i in range(5):
+	print(i)
